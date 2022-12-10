96033d732a25491e295665b372805ae29c2636df
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2022-12-10 15:17:18 +0800

    printBoard, BFS done

--- cuda_solver.cu
+++ cuda_solver.cu
@@ -3,10 +3,137 @@
 #include "sudoku.h"
 
 #define NUM_TILES N * N
+#define n SUB_N
 
 void load(Grid* grid, int *board) {
     for (int i = 0; i < NUM_TILES; i++)
         board[i] = (*grid)[i / N][i % N];
+}
+
+void printBoard(int *board) {
+    for (int i = 0; i < N; i++) {
+        if (i % n == 0) {
+            printf("-----------------------\n");
+        }
+
+        for (int j = 0; j < N; j++) {
+            if (j % n == 0) {
+            printf("| ");
+            }
+            printf("%d ", board[i * N + j]);
+        }
+
+        printf("|\n");
+    }
+    printf("-----------------------\n");
+}
+
+/**
+ * This kernel takes a set of old boards and finds all possible next boards by filling in the next
+ * empty space.
+ *
+ * old_boards:      This is an array of size sk. Each N * N section is another board. The rows
+ *                  are contiguous within the board. This array stores the previous set of boards.
+ *
+ * new_boards:      This is an array of size sk. Each N * N section is another board. The rows
+ *                  are contiguous within the board. This array stores the next set of boards.
+ *
+ * total_boards:    Number of old boards.
+ *
+ * board_index:     Index specifying the index of the next opening in new_boards.
+ *
+ * empty_spaces:    This is an array of size sk. Each N * N section is another board, storing the
+ *                  indices of empty spaces in new_boards.
+ *
+ * empty_space_count:   This is an array of size sk / N / N + 1 which stores the number of empty
+ *                      spaces in the corresponding board.
+ */
+__global__
+void
+cudaBFSKernel(int *old_boards,
+        int *new_boards,
+        int total_boards,
+        int *board_index,
+        int *empty_spaces,
+        int *empty_space_count) {
+    
+    unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    // board_index must start at zero 
+
+    while (index < total_boards) {
+        // find the next empty spot
+        int found = 0;
+
+        for (int i = (index * N * N); (i < (index * N * N) + N * N) && (found == 0); i++) {
+            // found a open spot
+            if (old_boards[i] == 0) {
+                found = 1;
+                // get the correct row and column shits
+                int temp = i - N * N * index;
+                int row = temp / N;
+                int col = temp % N;
+                
+                // figure out which numbers work here
+                for (int attempt = 1; attempt <= N; attempt++) {
+                    int works = 1;
+                    // row constraint, test various columns
+                    for (int c = 0; c < N; c++) {
+                        if (old_boards[row * N + c + N * N * index] == attempt) {
+                            works = 0;
+                        }
+                    }
+                    // column contraint, test various rows
+                    for (int r = 0; r < N; r++) {
+                        if (old_boards[r * N + col + N * N * index] == attempt) {
+                            works = 0;
+                        }
+                    }
+                    // box constraint
+                    for (int r = n * (row / n); r < n; r++) {
+                        for (int c = n * (col / n); c < n; c++) {
+                            if (old_boards[r * N + c + N * N * index] == attempt) {
+                                works = 0;
+                            }
+                        }
+                    }
+                    if (works == 1) {
+                        // copy the whole board
+
+                        int next_board_index = atomicAdd(board_index, 1);
+                        int empty_index = 0;
+                        for (int r = 0; r < 9; r++) {
+                            for (int c = 0; c < 9; c++) {
+                                new_boards[next_board_index * 81 + r * 9 + c] = old_boards[index * 81 + r * 9 + c];
+                                if (old_boards[index * 81 + r * 9 + c] == 0 && (r != row || c != col)) {
+                                    empty_spaces[empty_index + 81 * next_board_index] = r * 9 + c;
+
+                                    empty_index++;
+                                }
+                            }
+                        }
+                        empty_space_count[next_board_index] = empty_index;
+                        new_boards[next_board_index * 81 + row * 9 + col] = attempt;
+                    }
+                }
+            }
+        }
+
+        index += blockDim.x * gridDim.x;
+    }
+}
+
+
+void callBFSKernel(const unsigned int blocks, 
+                        const unsigned int threadsPerBlock,
+                        int *old_boards,
+                        int *new_boards,
+                        int total_boards,
+                        int *board_index,
+                        int *empty_spaces,
+                        int *empty_space_count) {
+    cudaBFSKernel<<<blocks, threadsPerBlock>>>
+        (old_boards, new_boards, total_boards, board_index, empty_spaces, empty_space_count);
 }
 
 void hostFE(Grid* grid){
