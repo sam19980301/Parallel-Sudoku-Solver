f3b55a0048b46b5db622d302580d0d7754cb4b3d
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2022-12-10 15:24:25 +0800

    validBoard done

--- cuda_solver.cu
+++ cuda_solver.cu
@@ -29,6 +29,154 @@ void printBoard(int *board) {
 }
 
 /**
+ * This device checks the entire board to see if it is valid.
+ *
+ * board: this is a N * N sized array that stores the board to check. Rows are stored contiguously,
+ *        so to access row r and col c, use board[r * N + c]
+ */
+__device__
+bool validBoard(const int *board) {
+    bool seen[N];
+    clearBitmap(seen, N);
+
+    // check if rows are valid
+    for (int i = 0; i < N; i++) {
+        clearBitmap(seen, N);
+
+        for (int j = 0; j < N; j++) {
+            int val = board[i * N + j];
+
+            if (val != 0) {
+                if (seen[val - 1]) {
+                    return false;
+                } else {
+                    seen[val - 1] = true;
+                }
+            }
+        }
+    }
+
+    // check if columns are valid
+    for (int j = 0; j < N; j++) {
+        clearBitmap(seen, N);
+
+        for (int i = 0; i < N; i++) {
+            int val = board[i * N + j];
+
+            if (val != 0) {
+                if (seen[val - 1]) {
+                    return false;
+                } else {
+                    seen[val - 1] = true;
+                }
+            }
+        }
+    }
+
+    // finally check if the sub-boards are valid
+    for (int ridx = 0; ridx < n; ridx++) {
+        for (int cidx = 0; cidx < n; cidx++) {
+            clearBitmap(seen, N);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int val = board[(ridx * n + i) * N + (cidx * n + j)];
+
+                    if (val != 0) {
+                        if (seen[val - 1]) {
+                            return false;
+                        } else {
+                            seen[val-1] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    // if we get here, then the board is valid
+    return true;
+}
+
+/**
+ * This function takes a board and an index between 0 and N * N - 1. This function assumes the board
+ * without the value at changed is valid and checks for validity given the new change.
+ *
+ * board:   this is a N * N sized array that stores the board to check. Rows are stored
+ *          contiguously, so to access row r and col c, use board[r * N + c]
+ *
+ * changed: this is an integer that stores the index of the board that was changed
+ */
+__device__
+bool validBoard(const int *board, int changed) {
+
+    int r = changed / 9;
+    int c = changed % 9;
+
+    // if changed is less than 0, then just default case
+    if (changed < 0) {
+        return validBoard(board);
+    }
+
+    if ((board[changed] < 1) || (board[changed] > 9)) {
+        return false;
+    }
+
+    bool seen[N];
+    clearBitmap(seen, N);
+
+    // check if row is valid
+    for (int i = 0; i < N; i++) {
+        int val = board[r * N + i];
+
+        if (val != 0) {
+            if (seen[val - 1]) {
+                return false;
+            } else {
+                seen[val - 1] = true;
+            }
+        }
+    }
+
+    // check if column is valid
+    clearBitmap(seen, N);
+    for (int j = 0; j < N; j++) {
+        int val = board[j * N + c];
+
+        if (val != 0) {
+            if (seen[val - 1]) {
+                return false;
+            } else {
+                seen[val - 1] = true;
+            }
+        }
+    }
+
+    // finally check if the sub-board is valid
+    int ridx = r / n;
+    int cidx = c / n;
+
+    clearBitmap(seen, N);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int val = board[(ridx * n + i) * N + (cidx * n + j)];
+
+            if (val != 0) {
+                if (seen[val - 1]) {
+                    return false;
+                } else {
+                    seen[val - 1] = true;
+                }
+            }
+        }
+    }
+
+    // if we get here, then the board is valid
+    return true;
+}
+
+/**
  * This kernel has each thread try to solve a different board in the input array using the
  * backtracking algorithm.
  *
